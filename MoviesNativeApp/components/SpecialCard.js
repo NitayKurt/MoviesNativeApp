437080dafc5a73219efb0b74368ac144083625e7
NitayKurt <nitay121@gmail.com>
2023-05-28 18:46:46 +0300

    new data

---
+++ MoviesNativeApp/components/SpecialCard.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SpecialCard() {
+  return (
+    <View>
+      <Text>SpecialCard</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
