185de8f1f44fd321ef539155ee157058870cf3f7
NitayKurt <nitay121@gmail.com>
2023-05-28 23:48:21 +0300

    new data

--- MoviesNativeApp/App.js
+++ MoviesNativeApp/App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Home from './screens/Home';
+import Favourites from './screens/Favourites';
 
 export default function App() {
+
+  const Tab = createBottomTabNavigator();
+
+  //Screen names
+  const homeName= 'Home';
+  const favouritesName= 'Favourites';
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({route})=>({
+        tabBarIcon:({focused, color, size})=>{
+          let iconName;
+          if(route.name===homeName){
+            iconName=focused?'home':'home-outline';
+          }
+          else if(route.name===favouritesName){
+            iconName=focused?'heart':'heart-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        }
+      })}>
+        <Tab.Screen name="Home" component={Home} options={{tabBarActiveBackgroundColor:"aqua"}}  />
+        <Tab.Screen name="Favourites" component={Favourites} options={{tabBarActiveBackgroundColor:"aqua"}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
--- MoviesNativeApp/screens/Home.js
+++ MoviesNativeApp/screens/Home.js
@@ -1,12 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Avatar, Button, Card, Searchbar } from 'react-native-paper';
+import { useState } from 'react';
+
+const LeftContent = props => <Avatar.Icon {...props} icon="movie" />
 
 export default function Home() {
+
+  const [data, setData] = useState([]);
+
+  const getMoviesFromApi = async () => {
+    try {
+      let response = await fetch(
+        'http://www.omdbapi.com/?apikey=6a3c2a1c&s=batman',
+      );
+      let json = await response.json();
+      setData(json.Search);
+      console.log(data)
+      return json.Search;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const just = () =>{
+    console.log(`pressed`)
+  }
+
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Searchbar collapsable={true} onEndEditing={getMoviesFromApi}></Searchbar>
+      <ScrollView>
+        <Card>
+          <Card.Title title={"Movie Name"} subtitle={"YEAR"} left={LeftContent} />
+            <Card.Content>
+              <Text variant="titleLarge">Description</Text>
+              <Text variant="bodyMedium">Rate</Text>
+            </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} ></Card.Cover>
+        <Card.Actions>
+            <Button>Move to website</Button>
+            <Button style={styles.favBotton} onPress={just}>Add To Favourites</Button>
+        </Card.Actions>
+      </Card>
+      </ScrollView>
+  </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  favBotton:{
+    backgroundColor:"green",
+    color:"white",
+    borderRadius:10,
+    marginLeft:10,
+    
+  },
+})
